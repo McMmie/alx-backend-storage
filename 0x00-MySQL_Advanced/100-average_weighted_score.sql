7e84205cfe5262140c0f4300aa2b2a0d4e60f266
McMmie <tharausamuel01@gmail.com>
2024-03-21 17:44:04 +0000

    fixed typo

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,24 +5,24 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-	DECLARE weighed_score INT DEFAULT 0;
+	DECLARE weighted_score INT DEFAULT 0;
 	DECLARE weight INT DEFAULT 0;
 
 	SELECT SUM(corrections.score * projects.weight)
-	INTO weighed_score
+	INTO weighted_score
 	FROM corrections
 	INNER JOIN projects
 	ON corrections.project_id = projects.id
 	WHERE correntions.user_id = user_id;
 
 	SELECT SUM(projects.weight)
-	INTO total_weight
+	INTO weight
 	FROM corrections
 	INNER JOIN projects
 	ON corrections.project_id = projects.id
 	WHERE corrections.user_id = user_id;
 
-	IF total_weight = 0 THEN
+	IF weight = 0 THEN
 		UPDATE users
 		SET users.average_score = 0
 		WHERE users.id = user_id;
