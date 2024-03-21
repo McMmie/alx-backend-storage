9690633fb54651c7e423998df61d83343f8b7d69
McMmie <tharausamuel01@gmail.com>
2024-03-21 09:47:20 +0000

    SQL files

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- a SQL script that creates a table users
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
